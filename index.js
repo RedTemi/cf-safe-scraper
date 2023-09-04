2035007272c6a7aadb511307d6a9a35be5f6b885
Temi <redtemi@icloud.com>
2023-09-04 11:27:52 +0100

    fixed

---
+++ index.js
@@ -0,0 +1,53 @@
+const { Builder, By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const fs = require("fs");
+const TwoCaptcha = require("2captcha");
+const { error } = require("console");
+
+// const solver = new TwoCaptcha('YOUR_API_KEY');
+const chromeOptions = new chrome.Options();
+chromeOptions.addArguments("--headless");
+chromeOptions.addArguments(
+  "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+);
+
+(async () => {
+  const driver = new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(chromeOptions)
+    .build();
+
+  try {
+    await driver.get("https://www.oddschecker.com/football");
+    await driver.sleep(5000);
+
+    console.log("here");
+
+    await driver.takeScreenshot().then((data) => {
+      fs.writeFileSync("./foo.png", data, "base64");
+    });
+
+    console.log("in try block");
+
+    await driver.sleep(30000);
+
+    driver
+      .findElements({
+        className: "OddsTableContainer_o16zgpun",
+      })
+      .then((elements) => {
+        elements.filter((value) => {
+          value.getText().then((str) => {
+            fs.appendFileSync("./results.txt", str + "\n");
+          });
+        });
+      })
+      .catch(error=>{
+        console.log('error caught',error)
+      });
+  } catch (error) {
+    console.log("unexpected schitz", error);
+  } finally {
+    await driver.quit();
+  }
+})();
--- test.py
+++ test.py
@@ -1,25 +1,32 @@
+import time
 from selenium import webdriver
+from twocaptcha import TwoCaptcha
 from selenium.webdriver.common.by import By
+
+solver = TwoCaptcha('YOUR_API_KEY')
+
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-caps = webdriver.DesiredCapabilities.CHROME.copy()
-caps['goog:loggingPrefs'] = {'performance': 'ALL'} 
 driver = webdriver.Chrome(options=options)
 
 driver.get("https://www.oddschecker.com/football")
 
-driver.get_screenshot_as_file("scr/foo.png")
+time.sleep(5)
 
-# Wait for Cloudflare to finish running
-for entry in driver.get_log('performance'):
-    if entry['message']['method'] == 'Network.responseReceived':
-        if 'cf-chl-bypass' in entry['message']['params']['response']['url']:
-            break
+print("here")
+driver.get_screenshot_as_file("./foo.png")
 
-# Scrape page here...
 
-# driver.get_screenshot_as_file("foo.png")
+if "Checking your browser" in driver.page_source:
+    # Wait for a few seconds and retry
+
+    time.sleep(5)
+    # continue here
+    time.sleep(10)
+else:
+    driver.find_element(By.CLASS_NAME,'inside-close-button').click()
+    driver.get_screenshot_as_file("./foo2.png")
 
 
 driver.quit()
